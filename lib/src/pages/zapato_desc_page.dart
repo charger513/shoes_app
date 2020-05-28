f5b46d970049fab35a0fccce65cf538adba03656
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-28 16:47:59 -0500

    Opciones finales

--- lib/src/pages/zapato_desc_page.dart
+++ lib/src/pages/zapato_desc_page.dart
@@ -7,7 +7,21 @@ class ZapatoDescPage extends StatelessWidget {
     return Scaffold(
         body: Column(
       children: <Widget>[
-        ZapatoSizePreview(fullScreen: true),
+        Stack(
+          children: <Widget>[
+            ZapatoSizePreview(fullScreen: true),
+            Positioned(
+              top: 80,
+              child: FloatingActionButton(
+                child: Icon(Icons.chevron_left, color: Colors.white, size: 60),
+                elevation: 0,
+                highlightElevation: 0,
+                backgroundColor: Colors.transparent,
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
         Expanded(
           child: SingleChildScrollView(
             child: Column(
@@ -18,13 +32,59 @@ class ZapatoDescPage extends StatelessWidget {
                       "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
                 ),
                 _MontoBuyNow(),
-                _ColoresYMas()
+                _ColoresYMas(),
+                _BotonesLikeCartSettings()
               ],
             ),
           ),
         ),
       ],
     ));
+  }
+}
+
+class _BotonesLikeCartSettings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 30),
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _BotonSombreado(Icon(Icons.favorite, color: Colors.red, size: 25)),
+          _BotonSombreado(Icon(Icons.shopping_cart,
+              color: Colors.grey.withOpacity(0.4), size: 25)),
+          _BotonSombreado(Icon(Icons.settings,
+              color: Colors.grey.withOpacity(0.4), size: 25)),
+        ],
+      ),
+    );
+  }
+}
+
+class _BotonSombreado extends StatelessWidget {
+  final Icon icon;
+
+  const _BotonSombreado(this.icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 55,
+      height: 55,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black38,
+                spreadRadius: -5,
+                blurRadius: 20,
+                offset: Offset(0, 10))
+          ]),
+      child: icon,
+    );
   }
 }
 
